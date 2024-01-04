feed8e2c1c136191785e931082e660f3cd0d0db4
TomShine <shangzongyu1111@gmail.com>
2024-01-04 22:10:34 +0800

    删除无用的 docs

--- docs/push.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-if __name__ == "__main__":
-    os.system("git add -A")
-    os.system("git commit")
-    os.system("git fetch origin")
-    os.system("git rebase origin/main")
-    os.system("git push origin main")
